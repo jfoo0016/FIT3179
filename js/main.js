80edcb7e726f9a92298e084e31de444e7f3472d0
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-06 15:59:30 +0800

    progress

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var spec2 = "js/visualisation.vl.json";
-vegaEmbed('#visualisation', spec2).then(function(result) {
+var spec2 = "js/choropleth_map.vl.json";
+vegaEmbed('#chropleth_map', spec2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec3 = "js/line_chart.vl.json";
+vegaEmbed('#line_chart', spec3).then(function(result) {1
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
